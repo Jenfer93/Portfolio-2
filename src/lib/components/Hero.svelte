4b85955532d41649ecbbcaea2931038bf4fbc967
Jenny Feragen <jenny@feragen.no>
2023-06-10 14:36:00 +0200

    added scrollbehavior

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -1,68 +1,68 @@
 <script lang="ts">
-import Down from '../images/1492533501-arrow-30_83308.svg';
+	import Down from '../images/1492533501-arrow-30_83308.svg';
 </script>
 
 <div>
-  <h1>Front End Developer</h1>
-  <h2>Jenny Feragen</h2>
-  <p>
-    Scroll down to learn more <br>
-    <a href="#myWork"><img src={Down} alt="down arrow"></a>
-  </p>
+	<h1>Front End Developer</h1>
+	<h2>Jenny Feragen</h2>
+	<p>
+		Scroll down to learn more <br />
+		<a href="#myWork"><img src={Down} alt="down arrow" /></a>
+	</p>
 </div>
 
 <style lang="scss">
-  div {
-      color: var(--primary);
-      min-height: 100vh;
-      text-align: center;
-      display: flex; 
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-    }
+	div {
+		color: var(--primary);
+		min-height: 100vh;
+		text-align: center;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
 
-    h1 {
-      font-size: var(--font-size-xxxl);
-      margin: var(--spacing-null);
-    }
+	h1 {
+		font-size: var(--font-size-xxxl);
+		margin: var(--spacing-null);
+	}
 
-    h2 {
-      font-size: var(--font-size-xl);
-    }
+	h2 {
+		font-size: var(--font-size-xl);
+	}
 
-    p{
-      font-style: italic;
-    }
+	p {
+		font-style: italic;
+	}
 
-    img {
-      width: 50px;
-      height: 50px;
-      animation: 2s linear infinite 0s bounce;
-    }
+	img {
+		width: 50px;
+		height: 50px;
+		animation: 2s linear infinite 0s bounce;
+	}
 
-    @keyframes bounce {
-    from {
-      /* pushes the sun down past the viewport */
-      transform: translateY(1vh);
-    }
-    to {
-      /* returns the sun to its default position */
-      transform: translateY(5vh);
-    }
-  }
+	@keyframes bounce {
+		from {
+			/* pushes the sun down past the viewport */
+			transform: translateY(1vh);
+		}
+		to {
+			/* returns the sun to its default position */
+			transform: translateY(5vh);
+		}
+	}
 
-  @media only screen and (max-width: 600px){
-    div {
-      min-height: 100vh;
-    }
+	@media only screen and (max-width: 600px) {
+		div {
+			min-height: 100vh;
+		}
 
-    h1 {
-      font-size: var(--font-size-xxl);
-    }
+		h1 {
+			font-size: var(--font-size-xxl);
+		}
 
-    h2 {
-      font-style: var(--font-size-m);
-    }
-  }
+		h2 {
+			font-style: var(--font-size-m);
+		}
+	}
 </style>
